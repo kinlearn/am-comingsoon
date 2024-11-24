cb28b12cf8ae3c6587e952cc10cf8ed9f89faf8a
Kin Learns <kin.learns@tuta.io>
2024-11-24 19:53:09 +0530

    Initial commit

---
+++ src/data/socials.ts
@@ -0,0 +1,33 @@
+export const socials = [
+  {
+    title: 'Facebook',
+    icon: 'mdi:facebook',
+    url: 'https://www.facebook.com/AksharManavSamuh/',
+  },
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'https://www.instagram.com/aksharmanav/',
+  },
+  {
+    title: 'Twitter',
+    icon: 'mdi:twitter',
+    url: 'https://x.com/AksharManav',
+  },
+  {
+    title: 'YouTube',
+    icon: 'mdi:youtube',
+    url: 'https://www.youtube.com/@AKSHARMANAV',
+  },
+
+  // {
+  //   title: 'GitHub',
+  //   icon: 'mdi:github',
+  //   url: 'https://github.com/littlesticksdev',
+  // },
+  // {
+  //   title: 'Dribbble',
+  //   icon: 'mdi:dribbble',
+  //   url: 'https://dribbble.com/littlesticksdev',
+  // }
+];
